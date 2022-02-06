74733439167229350c792a2a6e371198d51360bd
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2022-02-06 11:06:01 +0300

    feat: banner was added

---
+++ src/Components/Banner/Banner.tsx
@@ -0,0 +1,35 @@
+import "./Banner.scss";
+
+import React from "react";
+
+type BannerProps = {
+  cls: string[]
+  stopVideo: () => void
+};
+
+const Banner: React.FC<BannerProps> = ({cls, stopVideo}: BannerProps) => {
+
+
+  return (
+    <div className={cls.join(' ')}>
+      <p className="banner__title">
+        ИСПОЛНИТЕ МЕЧТУ ВАШЕГО БОССА!
+        ПОДАРИТЕ ЕМУ VOLVO!
+      </p>
+      <img src="../../assets/images/qrCode_pic.png" alt="QR-code" className="banner__qrCode"/>
+      <p className="banner__subtitle">
+        Сканируйте QR-код
+        или нажмите ОК
+      </p>
+      <button
+        type="button"
+        className="banner__btn"
+        onClick={stopVideo}
+      >
+        OK
+      </button>
+    </div>
+  );
+};
+
+export default Banner;
